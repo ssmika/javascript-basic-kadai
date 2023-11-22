6da5bd346e603ed2d90fc87ae70e95b42368a622
ssmika <147523894+ssmika@users.noreply.github.com>
2023-11-22 20:03:43 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date(); 
+
+const year = today.getFullYear();
+const month = today.getMonth();
+const day = today.getDate();
+
+console.log(year + '年' + (month + 1) + '月' + day + '日');
