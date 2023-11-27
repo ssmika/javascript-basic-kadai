bd517cd60acfa2d87120c91542865f8058593b55
ssmika <147523894+ssmika@users.noreply.github.com>
2023-11-27 09:06:26 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    const header2 = document.getElementById('text');
+    header2.textContent = 'ボタンをクリックしました';
+});    
